d3e4d599ba8b5def0f86cd21bc7b72804b98ad85
fasteque <info@fasteque.com>
2014-06-09 19:05:55 +0200

    #2: working on gridview multiple choice.

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/ui/RGBToolGalleryActivity.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/ui/RGBToolGalleryActivity.java
@@ -9,8 +9,11 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import butterknife.ButterKnife;
@@ -22,8 +25,7 @@ import com.fastebro.androidrgbtool.utils.UImage;
 
 public class RGBToolGalleryActivity extends Activity
     implements LoaderManager.LoaderCallbacks<Cursor>,
-        AdapterView.OnItemClickListener,
-        AdapterView.OnItemLongClickListener {
+        AdapterView.OnItemClickListener {
 
     @InjectView(R.id.grid_view)
     GridView mGridView;
@@ -40,7 +42,8 @@ public class RGBToolGalleryActivity extends Activity
 
         mGridView.setAdapter(mAdapter);
         mGridView.setOnItemClickListener(this);
-        mGridView.setOnItemLongClickListener(this);
+        mGridView.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
+        mGridView.setMultiChoiceModeListener(new MultiChoiceModeListener());
 
         getLoaderManager().initLoader(RGBToolImagesQuery.QUERY_ID,
                 null,
@@ -87,13 +90,41 @@ public class RGBToolGalleryActivity extends Activity
         }
     }
 
-    @Override
-    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        return false;
-    }
-
     interface RGBToolImagesQuery {
         int QUERY_ID = 1;
         Uri IMAGES_URI = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    }
+
+    public class MultiChoiceModeListener implements
+            GridView.MultiChoiceModeListener {
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            mode.setTitle("Select Items");
+            mode.setSubtitle("One item selected");
+            return true;
+        }
+
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return true;
+        }
+
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return true;
+        }
+
+        public void onDestroyActionMode(ActionMode mode) {
+        }
+
+        public void onItemCheckedStateChanged(ActionMode mode, int position,
+                                              long id, boolean checked) {
+            int selectCount = mGridView.getCheckedItemCount();
+            switch (selectCount) {
+                case 1:
+                    mode.setSubtitle("One item selected");
+                    break;
+                default:
+                    mode.setSubtitle("" + selectCount + " items selected");
+                    break;
+            }
+        }
     }
 }
