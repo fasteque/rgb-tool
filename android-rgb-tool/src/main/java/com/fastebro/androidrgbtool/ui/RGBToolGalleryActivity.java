059d215e877ba2c7f3432dba2f47b7ce2b0c19bb
fasteque <info@fasteque.com>
2014-06-09 14:58:51 +0200

    #2: implemented click on thumbnail.

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/ui/RGBToolGalleryActivity.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/ui/RGBToolGalleryActivity.java
@@ -3,20 +3,27 @@ package com.fastebro.androidrgbtool.ui;
 import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import com.fastebro.androidrgbtool.R;
 import com.fastebro.androidrgbtool.adapters.RGBToolImagesCursorAdapter;
+import com.fastebro.androidrgbtool.utils.UImage;
 
 
 public class RGBToolGalleryActivity extends Activity
-    implements LoaderManager.LoaderCallbacks<Cursor> {
+    implements LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener,
+        AdapterView.OnItemLongClickListener {
 
     @InjectView(R.id.grid_view)
     GridView mGridView;
@@ -32,6 +39,8 @@ public class RGBToolGalleryActivity extends Activity
         mAdapter = new RGBToolImagesCursorAdapter(this);
 
         mGridView.setAdapter(mAdapter);
+        mGridView.setOnItemClickListener(this);
+        mGridView.setOnItemLongClickListener(this);
 
         getLoaderManager().initLoader(RGBToolImagesQuery.QUERY_ID,
                 null,
@@ -62,9 +71,30 @@ public class RGBToolGalleryActivity extends Activity
         mAdapter.swapCursor(null);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Cursor cursor = (Cursor )parent.getItemAtPosition(position);
+
+        if(cursor != null) {
+            String photoPath = cursor.getString(cursor.getColumnIndex("_data"));
+
+            if(!"".equals(photoPath)) {
+                Intent colorPickerIntent = new Intent(this, ColorPickerActivity.class);
+                colorPickerIntent.putExtra(UImage.EXTRA_JPEG_FILE_PATH, photoPath);
+                colorPickerIntent.putExtra(UImage.EXTRA_DELETE_FILE, false);
+                startActivity(colorPickerIntent);
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return false;
+    }
+
     interface RGBToolImagesQuery {
         int QUERY_ID = 1;
-
         Uri IMAGES_URI = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
     }
 }
