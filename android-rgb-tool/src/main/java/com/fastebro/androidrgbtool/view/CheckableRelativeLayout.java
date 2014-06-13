554920e55190594b6a3e9ee62b179577912372d7
fasteque <info@fasteque.com>
2014-06-13 20:45:54 +0200

    #2: implemented checkable RelativeLayout.

---
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/view/CheckableRelativeLayout.java
@@ -0,0 +1,48 @@
+package com.fastebro.androidrgbtool.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+import android.widget.RelativeLayout;
+import com.fastebro.androidrgbtool.R;
+
+/**
+ * Created by danielealtomare on 13/06/14.
+ */
+public class CheckableRelativeLayout extends RelativeLayout implements Checkable {
+    private static final int[] STATE_CHECKABLE = {android.R.attr.state_pressed};
+    boolean checked = false;
+
+    public CheckableRelativeLayout(Context context) {
+        super(context);
+    }
+
+    public CheckableRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckableRelativeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+        refreshDrawableState();
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void toggle() {
+        setChecked(!checked);
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (checked) mergeDrawableStates(drawableState, STATE_CHECKABLE);
+
+        return drawableState;
+    }
+}
