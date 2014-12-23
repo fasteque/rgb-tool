0bdc601d015f71e12b1a68755daba7783a9420a3
fasteque <info@fasteque.com>
2014-12-23 18:05:12 +0100

    Implemented RecyclerView for picture selection list

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/adapters/SelectPictureListAdapter.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/adapters/SelectPictureListAdapter.java
@@ -1,6 +1,7 @@
 package com.fastebro.androidrgbtool.adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,44 +13,73 @@ import com.fastebro.androidrgbtool.R;
 /**
  * Created by danielealtomare on 21/06/14.
  */
-public class SelectPictureListAdapter extends ArrayAdapter {
-    private final LayoutInflater inflater;
+public class SelectPictureListAdapter extends RecyclerView.Adapter<SelectPictureListAdapter.ViewHolder> {
     private String[] entries;
+    private ItemClickListener listener;
 
-    public SelectPictureListAdapter(Context context,
-        int resource, String[] entries) {
-        super(context, resource, R.id.entry_title, entries);
-        this.inflater = LayoutInflater.from(context);
+    public SelectPictureListAdapter(String[] entries, ItemClickListener listener) {
         this.entries = entries;
-
+        this.listener = listener;
     }
 
     @Override
-    public int getCount() {
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.
+                from(viewGroup.getContext()).
+                inflate(R.layout.select_picture_row, viewGroup, false);
+
+        return new ViewHolder(itemView, new ItemClickListener() {
+            @Override
+            public void onClick(View v, int position, boolean isLongClick) {
+                listener.onClick(v, position, isLongClick);
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        switch (i) {
+            case 0:
+                viewHolder.icon.setImageResource(R.drawable.ic_action_content_picture);
+                viewHolder.title.setText(entries[0]);
+                break;
+            case 1:
+                viewHolder.icon.setImageResource(R.drawable.ic_action_device_access_sd_storage);
+                viewHolder.title.setText(entries[1]);
+                break;
+            case 2:
+                viewHolder.icon.setImageResource(R.drawable.ic_action_device_access_camera);
+                viewHolder.title.setText(entries[2]);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
         return entries.length;
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View entry = inflater.inflate(R.layout.select_picture_row, parent, false);
-        ImageView icon = (ImageView) entry.findViewById(R.id.entry_icon);
-        TextView title = (TextView) entry.findViewById(R.id.entry_title);
 
-        switch (position) {
-            case 0:
-                icon.setImageResource(R.drawable.ic_action_content_picture);
-                title.setText(entries[0]);
-                break;
-            case 1:
-                icon.setImageResource(R.drawable.ic_action_device_access_sd_storage);
-                title.setText(entries[1]);
-                break;
-            case 2:
-                icon.setImageResource(R.drawable.ic_action_device_access_camera);
-                title.setText(entries[2]);
-                break;
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        protected ImageView icon;
+        protected TextView title;
+        protected ItemClickListener listener;
+
+        public ViewHolder(View v, ItemClickListener listener) {
+            super(v);
+            this.listener = listener;
+            icon = (ImageView) v.findViewById(R.id.entry_icon);
+            title = (TextView) v.findViewById(R.id.entry_title);
+            v.setOnClickListener(this);
         }
 
-        return entry;
+        @Override
+        public void onClick(View v) {
+            listener.onClick(v, getPosition(), false);
+        }
+    }
+
+    public interface ItemClickListener {
+        public void onClick(View v, int position, boolean isLongClick);
     }
 }
--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/fragments/SelectPictureDialogFragment.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/fragments/SelectPictureDialogFragment.java
@@ -2,6 +2,9 @@ package com.fastebro.androidrgbtool.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +13,17 @@ import android.widget.ListView;
 
 import com.fastebro.androidrgbtool.R;
 import com.fastebro.androidrgbtool.adapters.SelectPictureListAdapter;
+import com.fastebro.androidrgbtool.managers.RecyclerViewLinearLayoutManager;
 import com.fastebro.androidrgbtool.ui.MainActivity;
 
 /**
  * Created by danielealtomare on 21/06/14.
  */
-public class SelectPictureDialogFragment extends DialogFragment implements AdapterView.OnItemClickListener {
+public class SelectPictureDialogFragment extends DialogFragment implements SelectPictureListAdapter.ItemClickListener {
+    private RecyclerView recyclerView;
+    private SelectPictureListAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
 
     public SelectPictureDialogFragment() { }
 
@@ -23,13 +31,14 @@ public class SelectPictureDialogFragment extends DialogFragment implements Adapt
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_select_picture, container);
 
-        SelectPictureListAdapter mAdapter = new SelectPictureListAdapter(getActivity(),
-                R.layout.select_picture_row,
-                getResources().getStringArray(R.array.pick_color_array));
+        adapter = new SelectPictureListAdapter(getResources().getStringArray(R.array.pick_color_array), this);
 
-        ListView listview = (ListView) view.findViewById(android.R.id.list);
-        listview.setAdapter(mAdapter);
-        listview.setOnItemClickListener(this);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        layoutManager = new RecyclerViewLinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         getDialog().setTitle(getString(R.string.pick_color));
 
@@ -37,7 +46,7 @@ public class SelectPictureDialogFragment extends DialogFragment implements Adapt
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onClick(View v, int position, boolean isLongClick) {
         switch (position) {
             case 0:
                 if (getActivity() != null) {
