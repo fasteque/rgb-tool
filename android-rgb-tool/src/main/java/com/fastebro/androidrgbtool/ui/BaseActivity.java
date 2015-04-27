945c79741a4b9f1f7a4b1b40302b94a9b08321e3
fasteque <info@fasteque.com>
2015-04-27 23:07:17 +0200

    issue #21: use the new AppCompatActivity

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/ui/BaseActivity.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/ui/BaseActivity.java
@@ -2,7 +2,7 @@ package com.fastebro.androidrgbtool.ui;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
 import android.transition.Fade;
 
@@ -11,7 +11,7 @@ import com.fastebro.androidrgbtool.R;
 /**
  * Created by danielealtomare on 26/10/14.
  */
-public class BaseActivity extends ActionBarActivity {
+public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
