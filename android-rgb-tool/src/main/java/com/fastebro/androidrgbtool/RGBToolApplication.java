3863ce820f06a90dfa86ac98c655131a99e84a39
fasteque <info@fasteque.com>
2015-05-08 22:24:24 +0200

    Added LeakCanary

---
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/RGBToolApplication.java
@@ -0,0 +1,25 @@
+package com.fastebro.androidrgbtool;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * Created by danielealtomare on 08/05/15.
+ */
+public class RGBToolApplication extends Application {
+
+    private RefWatcher refWatcher;
+
+    public static RefWatcher getRefWatcher(Context context) {
+        RGBToolApplication application = (RGBToolApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+
+    @Override public void onCreate() {
+        super.onCreate();
+        refWatcher = LeakCanary.install(this);
+    }
+}
