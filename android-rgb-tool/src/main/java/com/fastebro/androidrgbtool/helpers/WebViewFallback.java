a851dabad2c9cdc50c6304b009cc82583633ba8c
fasteque <daniele.altomare@gmail.com>
2015-10-04 20:02:02 +0200

    #53: implemented custom tabs helper classes

---
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/helpers/WebViewFallback.java
@@ -0,0 +1,18 @@
+package com.fastebro.androidrgbtool.helpers;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by danielealtomare on 04/10/15.
+ * Project: rgb-tool
+ */
+public class WebViewFallback implements CustomTabActivityHelper.CustomTabFallback {
+@Override
+public void openUri(Activity activity, Uri uri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(uri);
+        activity.startActivity(intent);
+        }
+        }
