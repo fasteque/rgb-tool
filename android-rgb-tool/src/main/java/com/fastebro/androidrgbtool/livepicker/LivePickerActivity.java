2def73f168fc157417725df27d50ee9f40342a0a
fasteque <daniele.altomare@gmail.com>
2016-03-26 16:34:37 +0100

    #76: stub files for live color picker

---
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/livepicker/LivePickerActivity.java
@@ -0,0 +1,15 @@
+package com.fastebro.androidrgbtool.livepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fastebro.androidrgbtool.R;
+
+public class LivePickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live_picker);
+    }
+}
