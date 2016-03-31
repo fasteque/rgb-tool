1e2524de56ec7f999fd4ead01a934be8dc4204d3
fasteque <daniele.altomare@gmail.com>
2016-03-31 19:54:33 +0200

    #76: stub class for custom TextureView

---
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/livepicker/LivePickerTextureView.java
@@ -0,0 +1,14 @@
+package com.fastebro.androidrgbtool.livepicker;
+
+import android.content.Context;
+import android.view.TextureView;
+
+/**
+ * Created by danielealtomare on 31/03/16.
+ * Project: rgb-tool
+ */
+public class LivePickerTextureView extends TextureView {
+    public LivePickerTextureView(Context context) {
+        super(context);
+    }
+}
