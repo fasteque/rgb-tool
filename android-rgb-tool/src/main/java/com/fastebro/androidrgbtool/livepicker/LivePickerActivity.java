d127ef8d25a535eba0f3d9775295a07778bab17f
fasteque <daniele.altomare@gmail.com>
2016-05-05 18:24:33 +0200

    #76: fixed autofocus camera settings

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/livepicker/LivePickerActivity.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/livepicker/LivePickerActivity.java
@@ -1,18 +1,22 @@
 package com.fastebro.androidrgbtool.livepicker;
 
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.PorterDuff;
 import android.hardware.Camera;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.fastebro.androidrgbtool.R;
 import com.fastebro.androidrgbtool.utils.CameraUtils;
 
-public class LivePickerActivity extends AppCompatActivity {
+public class LivePickerActivity extends AppCompatActivity implements LivePickerTextureView.OnColorPointedListener,
+        View.OnClickListener {
     private static final String TAG = LivePickerActivity.class.getName();
 
     private Camera camera;
@@ -20,6 +24,10 @@ public class LivePickerActivity extends AppCompatActivity {
     private FrameLayout livePreviewContainer;
     private LivePickerTextureView livePickerTextureView;
     private boolean isPortrait;
+    private int pointedColor;
+    private View pointerRing;
+
+    private boolean isFlashOn;
 
     private static Camera getFacingBackCameraInstance() {
         Camera camera = null;
@@ -60,8 +68,10 @@ public class LivePickerActivity extends AppCompatActivity {
     }
 
     private void initViews() {
+        // TODO: use ButterKnife
         isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
         livePreviewContainer = (FrameLayout) findViewById(R.id.live_picker_preview_container);
+        pointerRing = findViewById(R.id.live_picker_pointer_stroke);
     }
 
     private void releaseCameraPreview() {
@@ -79,43 +89,98 @@ public class LivePickerActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onColorPointed(int newColor) {
+        pointedColor = newColor;
+        pointerRing.getBackground().setColorFilter(pointedColor, PorterDuff.Mode.SRC_ATOP);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO
+    }
+
     private class CameraAsyncTask extends AsyncTask<Void, Void, Camera> {
+
+        protected FrameLayout.LayoutParams previewParams;
 
         @Override
         protected Camera doInBackground(Void... params) {
-            Camera camera = getFacingBackCameraInstance();
+            return getFacingBackCameraInstance();
+        }
 
-            if (camera == null) {
-                LivePickerActivity.this.finish();
-            } else {
-                /*
-                Camera.Parameters cameraParameters = camera.getParameters();
-                Camera.Size bestSize = CameraUtils.getBestPreviewSize(
-                        cameraParameters.getSupportedPreviewSizes()
-                        , livePreviewContainer.getWidth()
-                        , livePreviewContainer.getHeight()
-                        , isPortrait);
-                //set optimal camera preview
-                cameraParameters.setPreviewSize(bestSize.width, bestSize.height);
-                camera.setParameters(cameraParameters);
+        @Override
+        protected void onPostExecute(Camera camera) {
+            if (!isCancelled()) {
+                LivePickerActivity.this.camera = camera;
+                if (LivePickerActivity.this.camera == null) {
+                    LivePickerActivity.this.finish();
+                } else {
+                    Camera.Parameters cameraParameters = camera.getParameters();
+                    Camera.Size bestSize = CameraUtils.getBestPreviewSize(
+                            cameraParameters.getSupportedPreviewSizes()
+                            , livePreviewContainer.getWidth()
+                            , livePreviewContainer.getHeight()
+                            , isPortrait);
+                    // Set optimal camera preview
+                    cameraParameters.setPreviewSize(bestSize.width, bestSize.height);
+                    // Set focus mode
+                    if (cameraParameters.getSupportedFocusModes().contains(
+                            Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO)) {
+                        cameraParameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
+                    }
 
-                //set camera orientation to match with current device orientation
-                CameraUtils.setCameraDisplayOrientation(LivePickerActivity.this, camera);
+                    camera.setParameters(cameraParameters);
 
-                //get proportional dimension for the layout used to display preview according to the preview size used
-                int[] adaptedDimension = CameraUtils.getProportionalDimension(
-                        bestSize
-                        , livePreviewContainer.getWidth()
-                        , livePreviewContainer.getHeight()
-                        , isPortrait);
+                    // Set camera orientation to match with current device orientation
+                    CameraUtils.setCameraDisplayOrientation(LivePickerActivity.this, camera);
 
-                //set up params for the layout used to display the preview
-                mPreviewParams = new FrameLayout.LayoutParams(adaptedDimension[0], adaptedDimension[1]);
-                mPreviewParams.gravity = Gravity.CENTER;
-                */
+                    // Get proportional dimension for the layout used to display preview according to the preview size
+                    // used
+                    int[] adaptedDimension = CameraUtils.getProportionalDimension(
+                            bestSize
+                            , livePreviewContainer.getWidth()
+                            , livePreviewContainer.getHeight()
+                            , isPortrait);
+
+                    // Set up params for the layout used to display the preview
+                    previewParams = new FrameLayout.LayoutParams(adaptedDimension[0], adaptedDimension[1]);
+                    previewParams.gravity = Gravity.CENTER;
+
+                    // Set up camera preview
+                    livePickerTextureView = new LivePickerTextureView(LivePickerActivity.this, LivePickerActivity
+                            .this.camera);
+                    livePickerTextureView.setOnColorPointedListener(LivePickerActivity.this);
+                    livePickerTextureView.setOnClickListener(LivePickerActivity.this);
+
+                    livePreviewContainer.addView(livePickerTextureView, 0, previewParams);
+                }
             }
+        }
 
-            return camera;
+        @Override
+        protected void onCancelled(Camera camera) {
+            if (camera != null) {
+                camera.release();
+            }
+        }
+    }
+
+    private boolean isFlashSupported() {
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    private void toggleFlash() {
+        if (camera != null) {
+            final Camera.Parameters parameters = camera.getParameters();
+            final String flashParameter = isFlashOn ? Camera.Parameters.FLASH_MODE_OFF : Camera.Parameters
+                    .FLASH_MODE_TORCH;
+            parameters.setFlashMode(flashParameter);
+            camera.stopPreview();
+            camera.setParameters(parameters);
+            camera.startPreview();
+            isFlashOn = !isFlashOn;
+            invalidateOptionsMenu();
         }
     }
 }
