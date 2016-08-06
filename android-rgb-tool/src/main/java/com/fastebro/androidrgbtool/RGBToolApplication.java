335bccced89e6656a4d167b3a6cb333e0c8b3c39
fasteque <daniele.altomare@gmail.com>
2016-08-06 11:52:09 +0200

    Dependencies update

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/RGBToolApplication.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/RGBToolApplication.java
@@ -1,11 +1,6 @@
 package com.fastebro.androidrgbtool;
 
 import android.app.Application;
-import android.content.Context;
-import android.support.annotation.NonNull;
-
-import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
 
 import timber.log.Timber;
 
@@ -15,21 +10,11 @@ import timber.log.Timber;
  */
 public class RGBToolApplication extends Application {
 
-    private static final String LOG_TAG = "RGBTool";
-    private RefWatcher refWatcher;
-
-    public static RefWatcher getRefWatcher(@NonNull Context context) {
-        RGBToolApplication application = (RGBToolApplication) context.getApplicationContext();
-        return application.refWatcher;
-    }
-
     @Override public void onCreate() {
         super.onCreate();
-        refWatcher = LeakCanary.install(this);
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
-            Timber.tag(LOG_TAG);
         }
     }
 }
