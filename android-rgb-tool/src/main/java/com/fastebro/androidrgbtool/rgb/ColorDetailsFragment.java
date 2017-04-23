19661111ed8d142be6301c785d09fcfacb219a83
fasteque <daniele.altomare@gmail.com>
2017-04-23 21:39:30 +0200

    setting views and minimal binding

--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/rgb/ColorDetailsFragment.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/rgb/ColorDetailsFragment.java
@@ -1,6 +1,15 @@
 package com.fastebro.androidrgbtool.rgb;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fastebro.androidrgbtool.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by danielealtomare on 16/04/17.
@@ -8,4 +17,24 @@ import android.support.v4.app.Fragment;
  */
 
 public class ColorDetailsFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    public ColorDetailsFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_rgb_tab_details, container);
+        unbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
 }
--- android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/rgb/MainColorFragment.java
+++ android-rgb-tool/src/main/java/com/fastebro/androidrgbtool/rgb/MainColorFragment.java
@@ -1,6 +1,15 @@
 package com.fastebro.androidrgbtool.rgb;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fastebro.androidrgbtool.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by danielealtomare on 16/04/17.
@@ -8,4 +17,24 @@ import android.support.v4.app.Fragment;
  */
 
 public class MainColorFragment extends Fragment {
+
+    private Unbinder unbinder;
+
+    public MainColorFragment() { }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_rgb_tab_picker, container);
+        unbinder = ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
 }
