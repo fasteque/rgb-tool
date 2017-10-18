e8d8f1218dab740f4853fbaba81aa53993e19f54
Daniele Altomare <daniele.altomare@gmail.com>
2017-10-18 15:42:59 +0200

    message is optional, fixed crash

--- model/src/main/java/com/fastebro/androidrgbtool/model/events/PrintColorEvent.kt
+++ model/src/main/java/com/fastebro/androidrgbtool/model/events/PrintColorEvent.kt
@@ -4,4 +4,4 @@ package com.fastebro.androidrgbtool.model.events
  * Created by danielealtomare on 26/12/14.
  * Project: rgb-tool
  */
-class PrintColorEvent(val message: String)
+class PrintColorEvent(val message: String?)
